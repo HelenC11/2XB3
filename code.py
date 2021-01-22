c2d3f6235e98504a1daefc1acd8d7de366593ba1
Helen <Chenh214@mcmaster.ca>
2021-01-22 13:22:41 -0500

    Fix + random adversity spam

--- code.py
+++ code.py
@@ -1,9 +1,11 @@
 
 def are_valid_groups(listStudentNum, listOfGroups):
   result = True
-  for x in nums:
-       result = any(x in sl for sl in groups)
+  for x in listStudentNums:
+       result = any(x in sl for sl in listOfGroups)
        if result == False:
            return False
                 
   return result
+
+HAHAKJSKAJSLAJLSJALJKWNLKDNDJA"J"K"AKSLNKNSL:ASLKAS"KA"S:KALSMASM<MSLAKQLEKN><DA><
