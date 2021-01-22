175568dfc30a2b319f4c6b2968cf164f8179e4a7
Monica Tina Bazina-Grolinger <bazinagm@mills.cas.mcmaster.ca>
2021-01-22 13:45:05 -0500

    adding to are_valid_groups

--- code.py
+++ code.py
@@ -1,11 +1,17 @@
 
-def are_valid_groups(listStudentNum, listOfGroups):
-  result = True
-  for x in listStudentNums:
-       result = any(x in sl for sl in listOfGroups)
-       if result == False:
-           return False
-                
-  return result
+def are_valid_groups(nums, groups):
+    for group in groups:
+        if len(group) > 3 or len(group) < 2:
+            return False
+    seen = set()
+    for x in nums:
+        if x in seen: return False
+        seen.add(x)
 
-HAHAKJSKAJSLAJLSJALJKWNLKDNDJA"J"K"AKSLNKNSL:ASLKAS"KA"S:KALSMASM<MSLAKQLEKN><DA><
+    for x in nums:
+        result = any(x in sl for sl in groups)
+        if result == False:
+            return False
+
+    return result
+
