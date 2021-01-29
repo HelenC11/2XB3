ae18deff7b103904493ffd455db178baa41fedbf
bazinagm <bazinagm@mcmaster.ca>
2021-01-29 10:42:38 -0500

    adding copy and lookups to code.py

--- code.py
+++ code.py
@@ -1,17 +1,45 @@
+import timeit
 
-def are_valid_groups(nums, groups):
-    for group in groups:
-        if len(group) > 3 or len(group) < 2:
-            return False
-    seen = set()
-    for x in nums:
-        if x in seen: return False
-        seen.add(x)
+# time test copy
+def timeTestCopy(iterations,n):
+    totalTime = 0
+    for i in range(iterations):
+        listNum = list(range(n))
+        t0 = timeit.default_timer()
+        listNumCopy = listNum.copy()
+        t1 = timeit.default_timer()
+        totalTime += t1 - t0
+    return totalTime/iterations
 
-    for x in nums:
-        result = any(x in sl for sl in groups)
-        if result == False:
-            return False
+# output for the timeTestCopy
+def testCopy():
+    for i in range(1, 1000):
+        print(i*10, timeTestCopy(50, i*10))
 
-    return result
+# time test Lookups
+def timeTestLookups(iterations, n):
+    listNum = list(range(n))
+    for j in range(n):
+        totalTime = 0
+        for i in range(iterations):
+            t0 = timeit.default_timer()
+            elem = listNum[j]
+            t1 = timeit.default_timer()
+            totalTime += t1 - t0
+        print(j, totalTime/iterations)
 
+# output for Lookups
+def testLookups():
+    outF = open("out3.txt", "w")
+    def timeTestLookups(iterations, n):
+        listNum = list(range(n))
+        for j in range(n):
+            totalTime = 0
+            for i in range(iterations):
+                t0 = timeit.default_timer()
+                elem = listNum[j]
+                t1 = timeit.default_timer()
+                totalTime += t1 - t0
+            outF.write(str(j) + " " +  str(totalTime/iterations) + "\n" )
+    print(timeTestLookups(1000, 1000000))
+    outF.close()
